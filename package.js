5f8c75d2cef03ed3bca404291c895a0d5d8a1887
Krish Gupta <krshkun@proton.me>
2021-02-08 12:52:20 +0530

    :package: Package File For Meteor
    :package: Package.js File Is The Package File For Meteor (AtmosphereJS).

---
+++ package.js
@@ -0,0 +1,13 @@
+Package.describe({
+    name: 'krishdevdb:resetercss',
+    summary: 'Reset All CSS By Browsers With Reseter.css. And Make Web Look Same Across All Browsers',
+    version: '1.0.0',
+    git: 'https://github.com/krishdevdb/reseter.css.git'
+})
+
+Package.onUse(api => {
+    api.mainModule('css/reseter.min.css')
+    api.addFiles([
+        'css/reseter.css'
+    ], 'client')
+})
