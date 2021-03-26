540427e3729af0bb6fbb6a8e9929449111d1e02c
Krish Dev DB <krishdevdb@gmail.com>
2021-03-26 19:35:23 +0530

    :sparkles: Add sass Testing Support

---
+++ test/sass.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo "⏳ Installing Depedencies"
+npm i -g yarn
+yarn add sass postcss autoprefixer postcss-cli clean-css-cli bundlewatch
+echo "✅ Installed Dependencies"
+echo "⏳ Compiling SASS..."
+sass ../src/sass/reseter.sass build/sass/reseter.css --sourcemap=none
+echo "✅ Compiled SASS"
+echo "⏳ Adding Vendor Prefixes..."
+postcss build/sass/reseter.css -r --no-map -u autoprefixer
+echo "✅ Added Vendor Prefixes"
+echo "⏳ Minifing Output"
+cleancss build/sass/reseter.css -o build/sass/reseter.min.css
+echo "✅ Minified Output"
+echo "⏳ Compressing Output"
+bundlewatch --max-size 3kb build/sass/reseter.css build/sass/reseter.min.css
+echo "✅ Compressed Output"
