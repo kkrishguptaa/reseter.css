0c93532702d04aec10d37371a13947a81f266fb1
Krish Dev DB <krishdevdb@gmail.com>
2021-03-30 21:06:19 +0530

    :package: New Version Release

--- package.js
+++ package.js
@@ -3,7 +3,7 @@
 Package.describe({
     name: "krishdevdb:resetercss",
     summary: "Reset All CSS By Browsers With Reseter.css. And Make Web Look Same Across All Browsers",
-    version: "1.0.8",
+    version: "1.0.9",
     git: "https://github.com/krishdevdb/reseter.css.git"
 });
 
