9012276872292e107b4ab555b153a63cd52d80ce
Krish Gupta <krishdevdb@gmail.com>
2021-06-26 09:35:42 +0530

    feat: new build scripts

---
+++ .bundlewatch.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  files: [
+    {
+      path: "css/reseter.css",
+      maxSize: "1.5kb",
+    },
+    {
+      path: "css/reseter.min.css",
+      maxSize: "1kb",
+    },
+    {
+      path: "css/optionated.css",
+      maxSize: "1.5kb",
+    },
+    {
+      path: "css/optionated.min.css",
+      maxSize: "1kb",
+    },
+  ],
+  defaultCompression: "brotli",
+};
